ee80bcffa07ef84406ba3dae3ee797b669b0cd0f
JiuRanYa <1084350607@qq.com>
2024-06-20 17:15:19 +0800

    fix: treesitter autopairs

--- .config/nvim/lua/plugins/edit_support.lua
+++ .config/nvim/lua/plugins/edit_support.lua
@@ -1,36 +1,43 @@
 return {
-	{
-		"windwp/nvim-autopairs",
-		lazy = true,
-		config = function()
-			require("nvim-autopairs").setup({})
-		end,
-	},
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+    -- use opts = {} for passing setup options
+    -- this is equalent to setup({}) function
+  },
   {
     'phaazon/hop.nvim',
     branch = 'v2', -- optional but strongly recommended
     config = function()
       -- you can configure Hop the way you like here; see :h hop-config
-      require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+      require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
     end
   },
-	{
-		"Pocco81/auto-save.nvim",
-		event = "VeryLazy",
-		config = function()
-			require("auto-save").setup({
-				enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
-				debounce_delay = 200,
-			})
-		end,
-	},
-	{
-		"numToStr/Comment.nvim",
-		event = "VeryLazy",
-		config = function()
-			require("Comment").setup()
-		end,
-	},
-	{ "RRethy/vim-illuminate", event = "VeryLazy" },
-	{ "windwp/nvim-ts-autotag", event = "VeryLazy" },
+  {
+    "Pocco81/auto-save.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("auto-save").setup({
+        enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+        debounce_delay = 200,
+      })
+    end,
+  },
+  {
+    "numToStr/Comment.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("Comment").setup()
+    end,
+  },
+  { "RRethy/vim-illuminate", event = "VeryLazy" },
+  {
+    "windwp/nvim-ts-autotag",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,19 @@
-
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		event = "VeryLazy",
-    opts  = {
-			ensure_installed = { "javascript", "html", "json"},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = "VeryLazy",
+    dependencies = {
+      "windwp/nvim-ts-autotag"
+    },
+    opts = {
+      ensure_installed = { "javascript", "html", "json", "vue", "typescript" },
       -- 启用代码缩进模块 (=)
       indent = {
         enable = true,
       },
+      autotag = {
+        enable = true
+      },
     }
-	},
-  {
-    'windwp/nvim-ts-autotag',
-    event = "VeryLazy",
-    config = function ()
-      -- code
-      require('nvim-ts-autotag').setup({})
-    end
-  }
+  },
 }
