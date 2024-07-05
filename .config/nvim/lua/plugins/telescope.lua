4998e638ee311bb497f9a98fd183e6b8b87680e6
JiuRanYa <1084350607@qq.com>
2024-07-05 17:52:30 +0800

    chore: remove vim be good and telescope import

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -26,18 +26,6 @@ return {
           -- Add imports to the top of the file keeping the cursor in place
           insert_at_top = false,
           -- Support additional languages
-          custom_languages = {
-            {
-              -- The regex pattern for the import statement
-              regex = [[^(?:import(?:[\"'\s]*([\w*{}\n, ]+)from\s*)?[\"'\s](.*?)[\"'\s].*)]],
-              -- The Vim filetypes
-              filetypes = { "vue" },
-              -- The filetypes that ripgrep supports (find these via `rg --type-list`)
-              extensions = { "js" },
-              insert_at_line = 2,
-              insert_position = 2
-            },
-          },
         },
       },
     },
