4b52855f7cdbb525833ec902c7e79e63f712c706
JiuRanYa <1084350607@qq.com>
2024-07-08 16:48:00 +0800

    fix: telescope remove nvchad default config

--- .config/nvim/lua/plugins/edit_support.lua
+++ .config/nvim/lua/plugins/edit_support.lua
@@ -33,6 +33,16 @@ return {
     "windwp/nvim-ts-autotag",
     dependencies = "nvim-treesitter/nvim-treesitter",
     event = "VeryLazy",
+    config = function()
+      require('nvim-ts-autotag').setup({
+        opts = {
+          -- Defaults
+          enable_close = true,    -- Auto close tags
+          enable_rename = true,   -- Auto rename pairs of tags
+          enable_close_on_slash = false -- Auto close on trailing </
+        },
+      })
+    end
   },
   {
     "iamcco/markdown-preview.nvim",
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,25 +1,52 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	cmd = "Telescope",
-	keys = {
-		{ "<Leader>f", "<cmd>Telescope find_files hidden=true<CR>", {} },
-		{ "<Leader>o", ":Telescope lsp_document_symbols<CR>", {} },
-		{ "<Leader>rs", ":Telescope resume<CR>", {} },
-		{ "<C-q>", ":Telescope oldfiles<CR>", {} },
-	},
-	opts = function()
-		require("telescope").setup({
-			defaults = {
-				file_ignore_patterns = { "node_modules", "es", "lib", "dist" },
-			},
-			pickers = {
-				live_grep = {
-					additional_args = function()
-						return { "--hidden" }
-					end,
-				},
-			},
-		})
-	end,
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = "Telescope",
+    keys = {
+      { "<Leader>f",  "<cmd>Telescope find_files hidden=true<CR>", {} },
+      { "<Leader>o",  ":Telescope lsp_document_symbols<CR>",       {} },
+      { "<Leader>rs", ":Telescope resume<CR>",                     {} },
+      { "<C-q>",      ":Telescope oldfiles<CR>",                   {} },
+      { "fi",         ":Telescope import<CR>",                     {} },
+    },
+    opts = function()
+      require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = { "node_modules", "es", "lib", "dist" },
+        },
+        pickers = {
+          live_grep = {
+            additional_args = function()
+              return { "--hidden" }
+            end,
+          },
+        },
+        extensions = {
+          import = {
+            -- Add imports to the top of the file keeping the cursor in place
+            insert_at_top = false,
+            -- Support additional languages
+            custom_languages = {
+              {
+                -- The regex pattern for the import statement
+                regex = [[^(?:import(?:[\"'\s]*([\w*{}\n, ]+)from\s*)?[\"'\s](.*?)[\"'\s].*)]],
+                -- The Vim filetypes
+                filetypes = { "vue" },
+                -- The filetypes that ripgrep supports (find these via `rg --type-list`)
+                extensions = { "js" },
+              },
+            },
+          },
+        },
+      })
+    end,
+  },
+  {
+    'piersolenski/telescope-import.nvim',
+    dependencies = 'nvim-telescope/telescope.nvim',
+    config = function()
+      require("telescope").load_extension("import")
+    end
+  }
 }
