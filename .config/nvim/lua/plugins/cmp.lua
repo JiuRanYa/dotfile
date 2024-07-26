f21b742db4a5a7742045cc6dfe1cdf5103375b13
JiuRanYa <1084350607@qq.com>
2024-07-26 17:07:04 +0800

    chore: remove comment

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -14,7 +14,7 @@ return {
         mapping = cmp.mapping.preset.insert({
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
 
           ["<C-k>"] = cmp.mapping.select_prev_item(),
           -- 下一个
